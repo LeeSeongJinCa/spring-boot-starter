67e3937ae4e52e356eb3b87586f368c11b7c06a2
이성진 <chlskatnr1@naver.com>
2020-08-04 15:46:23 +0900

    [MODIFY]

--- src/test/java/com/hello/HelloApplicationTests.java
+++ src/test/java/com/hello/HelloApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.hello;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StarterApplicationTests {
+class HelloApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
